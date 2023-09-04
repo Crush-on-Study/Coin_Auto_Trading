27747c223089d961c0b0f1c02cb3e6ccd596ba08
Crush_on_Study <99119068+Crush-on-Study@users.noreply.github.com>
2023-09-04 14:34:50 +0900

    Update RSI.py
    RSI지수 + 볼린저밴드 값

--- autopjt/RSI.py
+++ autopjt/RSI.py
@@ -1,82 +1,32 @@
-import login,pyupbit
-import pandas as pd
+import pyupbit
+import talib
+import numpy as np
 import time
-import requests
+from get_top_5_volume import coin_list  # get_top_5_volume.py에서 가져온 상위 5개 코인 리스트
 
-upbit = pyupbit.Upbit(login.id,login.pw)
-
-# RSI 지수 구하는 함수
-## RSI : 주가의 상승과 하락의 상대적 강도를 나타내는 지표 -> 쉽게 말해서 과매수 구간인지? 과매도 구간인지?
-def get_rsi(symbol, interval, period):
-    df = pyupbit.get_ohlcv(symbol, interval=interval, count=period + 1)
-    delta = df['close'].diff()
-
-    gain = delta.where(delta > 0, 0)
-    loss = -delta.where(delta < 0, 0)  # 음수를 양수로 변환하여 계산
-    avg_gain = gain.rolling(window=period).mean()
-    avg_loss = loss.rolling(window=period).mean()
-
-    rs = avg_gain / (avg_loss + 1e-6)  # 0으로 나누는 것을 피하기 위해 작은 값을 더해줌
-    rsi = 100 - (100 / (1 + rs))
-
-    return rsi.iloc[-1]
-
-
-# 매수 함수
-def buy_logic():
-    pass
-
-# 매도 함수
-def sell_logic():
-    pass
-
-# 상위 5종목 코인 조회
-def get_top_tickers(count=5):
-    url = f"https://api.upbit.com/v1/market/all"
-    response = requests.get(url) # get방식으로 url 조회
-    data = response.json() # json 타입으로 데이터 전달
+# RSI와 볼린저 밴드 계산 함수
+def calculate_rsi_bollinger_bands(ohlcv):
+    close_prices = ohlcv['close'].values
     
-    markets = [{"market" : market["market"]} for market in data]
-    # 결과 : market : "krw-btc" , market : "krw-eth"... 이런 식이군.
-    tickers = [market["market"] for market in data if market["market"].startswith("KRW")]
+    # RSI 계산
+    rsi = talib.RSI(close_prices)
     
-    tickers_volumes = []
-    for chunk in [tickers[i:i+100] for i in range(0,len(tickers),100)]:
-        url = f"https://api.upbit.com/v1/ticker?markets={','.join(chunk)}"
-        response = requests.get(url)
-        data = response.json()
-        tickers_volumes.extend(data)
-        
-    sorted_tickers = sorted(tickers_volumes, key=lambda x: x["acc_trade_volume_24h"], reverse=True)
-    top_tickers = [ticker["market"] for ticker in sorted_tickers[:count]]
+    # 볼린저 밴드 계산
+    upper_band, middle_band, lower_band = talib.BBANDS(close_prices, timeperiod=20)
     
-    return top_tickers
-    
-    
-# 메인 함수
-def main():
-    interval = "minute1" # 1분봉
-    period = 14 # 14일 기준
-    buy_threshold = 30
-    sell_threshold = 70
-    volume_threshold = 5
-    
-    while True:
-        all5_tickers = get_top_tickers(volume_threshold) # 상위 5개 뽑아와.
-        for ticker in all5_tickers:
-            rsi = get_rsi(ticker,interval,period)
-            print(f'종목 : {ticker}, 현재 RSI 지수 : {rsi}')
+    return rsi[-1], upper_band[-1], middle_band[-1], lower_band[-1]
 
-            if rsi <= buy_threshold: # 매수 구간에 도달한 경우
-                # 그리고 그게 거래량 상위 5위 안에 드는 경우
-                # 매수 한다.
-                buy_logic()
-            
-            elif sell_threshold-10 <= rsi <= sell_threshold+10:
-                # 매도 한다.
-                sell_logic()
-        
-        time.sleep(10) # 10초에 1번씩 체크
-
-if __name__ == "__main__":
-    main()
+while True:
+    for coin in coin_list:
+        try:
+            ohlcv = pyupbit.get_ohlcv(coin, interval='day', count=21)
+            if ohlcv is not None and len(ohlcv) == 21:
+                rsi, upper_band, middle_band, lower_band = calculate_rsi_bollinger_bands(ohlcv)
+                print(f'{coin} - RSI: {rsi:.2f}, Upper Bollinger Band: {upper_band:.2f}, Middle Bollinger Band: {middle_band:.2f}, Lower Bollinger Band: {lower_band:.2f}')
+            else:
+                print(f'{coin} - 데이터 부족')
+        except Exception as e:
+            print(f'오류 발생: {e}')
+    
+    print("\n")
+    time.sleep(5)
