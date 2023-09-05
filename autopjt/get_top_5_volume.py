2c2757c689a0af97ee21eb052b0340d96e9c6606
Crush_on_Study <99119068+Crush-on-Study@users.noreply.github.com>
2023-09-05 11:10:55 +0900

    Update get_top_5_volume.py

--- autopjt/get_top_5_volume.py
+++ autopjt/get_top_5_volume.py
@@ -9,4 +9,5 @@ selector = "tbody > tr > td > div > a"
 columns = bs.select(selector)
  
 ticker_list = [x.text.strip().replace('/', '-') for x in columns if x.text.strip()]
-print(ticker_list[:5])
+real_ticker_list = ticker_list[:5]
+print(real_ticker_list)
