fb75dee2dae428235b9658d3334e1d64d515fff9
Crush_on_Study <99119068+Crush-on-Study@users.noreply.github.com>
2023-09-07 12:00:35 +0900

    Update rsi_backtest.py

--- autopjt/rsi_backtest.py
+++ autopjt/rsi_backtest.py
@@ -1,13 +1,16 @@
 import os
 import pandas as pd
 import ta
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # 현재 스크립트 파일의 디렉터리 경로
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # csv_folder 폴더 경로 (AUTOPJT 폴더 안에 있는 경우)
 csv_folder = os.path.join(script_dir, 'csv_folder')
+
+# 현재 날짜 구하기
+today_date = datetime.now().strftime('%Y-%m-%d')
 
 # RSI 전략 정의    
 def rsi_strategy(data):
@@ -57,30 +60,45 @@ def rsi_strategy(data):
 # CSV 폴더에서 파일 목록 가져오기
 csv_files = [f for f in os.listdir(csv_folder) if f.endswith('.csv')]
 
+# 오늘과 다른 날짜의 파일을 삭제
+for file_name in csv_files:
+    # 파일 이름에서 날짜 추출
+    file_date = file_name.split(' ')[0]
+    
+    if file_date != today_date:
+        file_path = os.path.join(csv_folder, file_name)  # 파일 경로
+        os.remove(file_path)
+        print(f'{file_name} 파일을 삭제했습니다.')
+    else:
+        print(f'{file_name}은 오늘의 데이터입니다. 백테스팅을 실행합니다.')
+
 # 파일별로 백테스팅 수행
 for file_name in csv_files:
-    file_path = os.path.join(csv_folder, file_name)  # 파일 경로
-    coin_name = file_name.split(' ')[1].split('.')[0]  # 코인 이름 추출
+    # 파일 이름에서 날짜 추출
+    file_date = file_name.split(' ')[0]
     
-    # CSV 파일 불러오기
-    try:
-        df = pd.read_csv(file_path)
+    if file_date == today_date:
+        file_path = os.path.join(csv_folder, file_name)  # 파일 경로
+        coin_name = file_name.split(' ')[1].split('.')[0]  # 코인 이름 추출
+        # CSV 파일 불러오기
+        try:
+            df = pd.read_csv(file_path)
 
-        # 컬럼 이름 변경
-        df.rename(columns={"Unnamed: 0": "timestamp"}, inplace=True)
+            # 컬럼 이름 변경
+            df.rename(columns={"Unnamed: 0": "timestamp"}, inplace=True)
 
-        # 백테스팅 수행
-        result = rsi_strategy(df)
-        
-        if result is not None:
-            final_balance, trade_log = result
+            # 백테스팅 수행
+            result = rsi_strategy(df)
+            
+            if result is not None:
+                final_balance, trade_log = result
 
             # 결과 출력
             print(f'{coin_name} 백테스팅 결과 - 최종 자본금: {final_balance}')
             print('거래 로그:')
             for action, timestamp, price in trade_log:
                 print(f"{timestamp}: {action} at {price}")
-        else:
-            print(f'{coin_name} 데이터에 대한 백테스팅 결과가 없습니다.')
-    except Exception as e:
-        print(f'오류 발생: {e}')
+            else:
+                print(f'{coin_name} 데이터에 대한 백테스팅 결과가 없습니다.')
+        except Exception as e:
+            print(f'오류 발생: {e}')
