7473285bf02df477e5d736d6da1add42271ec791
Crush_on_Study <99119068+Crush-on-Study@users.noreply.github.com>
2024-04-18 15:33:32 +0900

    Update ticker_Anal.py

--- Analysis/ticker_Anal.py
+++ Analysis/ticker_Anal.py
@@ -4,6 +4,7 @@ import requests
 from openpyxl import load_workbook
 from openpyxl.drawing.image import Image
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 # 이 함수는 업비트 API에서 실시간 호가 정보를 가져옴.
 # API 키가 필요 없으므로 headers는 생략.
@@ -17,7 +18,7 @@ def get_current_price_from_upbit(ticker):
     return data[0]['trade_price'] if data else None
 
 # 이 함수는 엑셀 파일에서 데이터를 읽고, 현재 가격 정보를 추가하여 수익률과 현재 수익금을 계산.
-def calculate_return_and_profit(file_path):
+def calculate_return_and_profit(file_path,current_date):
     df = pd.read_excel(file_path, engine='openpyxl')
     
     # 현재 가격, 수익률, 현재 수익금을 저장할 빈 리스트를 생성
@@ -104,9 +105,11 @@ def save_and_insert_chart_with_values(df, excel_file_path):
 # 함수 호출 부분.
 if __name__ == '__main__':
     file_path = r'경로 기입'
-    updated_file_path = calculate_return_and_profit(file_path)
+    # 현재 날짜를 연/월/일 포맷으로
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    updated_file_path = calculate_return_and_profit(file_path,current_date)
 
-    # 업데이트된 파일에서 데이터를 읽어옵니다.
+     # 업데이트된 파일에서 데이터를 읽어.
     df_updated = pd.read_excel(updated_file_path, engine='openpyxl')
-    # 시각화 함수를 호출합니다.
+    # 시각화 함수를 호출.
     save_and_insert_chart_with_values(df_updated, updated_file_path)
